8fca5f8c83cce7cab15318abd9a0bd5fd4028242
Scythe <rajpawar220207@gmail.com>
2026-02-25 21:37:51 +0530

    Add files via upload

---
+++ Q3_sq_cube.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//Q3.Scan 2 numbers from user.Print square and cube of both numbers.
+	
+	int num1,num2;
+	
+	cout<<"\nEnter 2 integers=";
+	cin>>num1>>num2;
+	
+	cout<<"\nSquare of Num1 ="<<num1*num1<<"\nCube of Num1 = "<<num1*num1*num1;
+	cout<<"\nSquare of Num2 ="<<num2*num2<<"\nCube of Num2 = "<<num2*num2*num2;
+	
+	return 0;
+}
