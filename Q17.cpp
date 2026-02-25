2a070a29a4cc83300480e6584c99514a11734546
Scythe <rajpawar220207@gmail.com>
2026-02-25 21:45:20 +0530

    Add files via upload

---
+++ Q17.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//Q17.WAP to print ascii value of any character.
+	char ch;
+	cout<<"\nEnter any character to know its ascii value =";
+	cin>>ch;
+	
+	cout<<"\nThe Ascii value of "<<ch<<" is "<<(int)ch;
+	
+	return 0;
+}
