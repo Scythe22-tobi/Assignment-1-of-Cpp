fc964e377e8d2e9ece8e7188027e36f7a8a4c8f2
Scythe <rajpawar220207@gmail.com>
2026-02-25 21:37:01 +0530

    Add files via upload

---
+++ Q1_avg.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//Q1.Scan 3 integers from user.Print their average.
+	
+	int num1,num2,num3;
+	int i;
+	cout<<"Enter 3 integers =";
+	
+	cin>>num1>>num2>>num3;
+	
+	cout<<"\nAverage = "<<(num1+num2+num3)/3.0;
+	
+	return 0;
+}
